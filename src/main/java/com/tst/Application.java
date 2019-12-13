65c6e244144b544805da9a7844d4fc71910f220a
Lourence <johnlourenceg@gmail.com>
2019-12-13 17:14:02 +0800

    added accounts receivable report generator

--- src/main/java/com/tst/Application.java
+++ src/main/java/com/tst/Application.java
@@ -2,10 +2,11 @@ package com.tst;
 
 public class Application {
     public static void main(String[]args){
-        new AccountReport().start();
-        new AccountLedgerReport().start();
-        new PdfGenerator().start();
-        new YTDConsumptionReport().start();
+//        new AccountReport().start();
+        new AccountsReceivableReport().start();
+//        new AccountLedgerReport().start();
+//        new PdfGenerator().start();
+//        new YTDConsumptionReport().start();
    }
 
 }
